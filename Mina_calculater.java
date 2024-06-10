2e9d858c2233f149d19ff1930dbe664a533c31e5
minanesem02 <gmina8143@gmail.com>
2024-06-11 00:54:22 +0300

    Add files via upload

---
+++ Mina_calculater.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mina_calculater;
+
+/**
+ *
+ * @author user
+ */ 
+
+public class Mina_calculater {
+
+    public static void main(String[] args) {
+      new Jframe_calc().setVisible(true);
+    }
+}
---
+++ txtAll.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mina_calculater;
+
+/**
+ *
+ * @author user
+ */
+class txtAll {
+
+    static String getText() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    static void setText(String All) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
